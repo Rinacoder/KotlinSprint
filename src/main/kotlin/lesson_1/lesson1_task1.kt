2baceabb6a03d631b91069beffe44f50289ce08e
rina <iraledy@yandex.ru>
2025-08-20 08:05:39 +0300

    KS-1-1 Исправление задачи 1 к Уроку 1. Удаление вывода переменных

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -3,7 +3,4 @@ package org.example.lesson_1
 fun main() {
     val numberOfOrders: Int = 75
     val gratitudeForPurchase: String = "Thank you for purchase!"
-
-    println(numberOfOrders)
-    println(gratitudeForPurchase)
 }
