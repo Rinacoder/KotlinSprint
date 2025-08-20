38da75ed4bbffa481c3d2eb2e3d6a1bfcab16773
rina <iraledy@yandex.ru>
2025-08-20 08:26:29 +0300

    KS-1-5 Исправление задачи 5 к Уроку 1. Изменение вывода времени. Добавление констант для перевода времени. Изменение логики перевода времени

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,25 +1,15 @@
 package org.example.lesson_1
 
+const val secondsInHour = 3600
+const val secondsInMinute = 60
+
 fun main() {
 
     var seconds: Int = 6480
-    val hour: Int = seconds / 3600
-    seconds %=  3600
-    val minute: Int = seconds / 60
-    seconds %=  60
+    val hour: Int = seconds / secondsInHour
+    seconds %=  secondsInHour
+    val minute: Int = seconds / secondsInMinute
+    seconds %=  secondsInMinute
+    println(String.format("%02d:%02d:%02d", hour, minute, seconds))
 
-    if(hour > 9)
-        print(hour)
-    else
-        print("0$hour")
-    print(":")
-    if(minute > 9)
-        print(minute)
-    else
-        print("0$minute")
-    print(":")
-    if(seconds > 9)
-        print(seconds)
-    else
-        print("0$seconds")
 }
