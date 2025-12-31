d3d3764e8f81cf97f0967253e4702a0a7745f81d
rina <iraledy@yandex.ru>
2025-12-31 17:27:39 +0300

    KS-2-1 решение задачи 4 к Уроку 2

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_4
+
+fun main() {
+    val crystalOre = 7
+    val ironOre = 11
+    val buff = 20
+    val crystalOresBuff = crystalOre * buff / 100
+    val ironOresBuff = ironOre * buff / 100
+    println( "buff crystal = $crystalOresBuff")
+    println( "buff iron = $ironOresBuff")
+}
