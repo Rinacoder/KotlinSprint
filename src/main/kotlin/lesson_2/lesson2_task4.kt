1a97d5c5d072baada2f50fdd018650ff1dc2afeb
rina <iraledy@yandex.ru>
2025-12-31 17:29:39 +0300

    KS-2-1 Изменение задачи 4 к Уроку 2, переименование переменной

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,9 +3,9 @@ package org.example.lesson_4
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buff = 20
-    val crystalOresBuff = crystalOre * buff / 100
-    val ironOresBuff = ironOre * buff / 100
+    val buffPercent = 20
+    val crystalOresBuff = crystalOre * buffPercent / 100
+    val ironOresBuff = ironOre * buffPercent / 100
     println( "buff crystal = $crystalOresBuff")
     println( "buff iron = $ironOresBuff")
 }
