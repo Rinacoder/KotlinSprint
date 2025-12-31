cfd16fd05d49d0ce39e5a52766b5e767ae67c71c
rina <iraledy@yandex.ru>
2025-12-31 15:39:22 +0300

    KS-2-1 изменение задачи 1 к Уроку 2

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,6 +1,7 @@
 package org.example.lesson_2
 
 fun main() {
-    val averageScore = (3 + 4 + 3 + 5)/4f
+    val scoreStudent = listOf(3, 4, 3, 5)
+    val averageScore = scoreStudent.sum().toDouble()/scoreStudent.count()
     println( String.format("Average students score: %.2f", averageScore))
 }
