7de9684e607670a2cadb136e211b1ea6c4b8f524
rina <iraledy@yandex.ru>
2026-01-02 18:06:17 +0300

    KS-3-2 Решение задачи 2 к Уроку 3

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    var lastname  = "Андреева"
+    val firstname  = "Татьяна"
+    val patronymic = "Сергеевна"
+    var age = 20
+    println( "[$lastname $firstname $patronymic, $age]")
+    age = 22
+    lastname  = "Сидорова"
+    println( "[$lastname $firstname $patronymic, $age]")
+
+}
