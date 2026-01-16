37dcb7d56ce9a3804f59bc354389a8b3b9bb7baf
rina <iraledy@yandex.ru>
2026-01-16 23:02:23 +0300

    KS-4-1 Исправление задачи 1 к Уроку 4, корректировка названия костанты

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_4
 
-val Total_Tables = 13
+val TOTAL_TABLES = 13
 
 fun main() {
     val TablesBookedToday = 13
     val TablesBookedTomorrow = 9
-    val availableToday = Total_Tables > TablesBookedToday
-    val availableTomorrow = Total_Tables > TablesBookedTomorrow
+    val availableToday = TOTAL_TABLES > TablesBookedToday
+    val availableTomorrow = TOTAL_TABLES > TablesBookedTomorrow
 
     println(
         "[Availability of tables for today: $availableToday],\n" +
