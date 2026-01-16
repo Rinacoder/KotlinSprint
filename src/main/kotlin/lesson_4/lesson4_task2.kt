597e315a258589c311cda022b133c06e80f1f763
rina <iraledy@yandex.ru>
2026-01-16 23:03:39 +0300

    KS-4-2 Исправление задачи 2 к Уроку 4, корректировка названия костанты

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_4
 
-const val Min_Weight = 35
-const val Max_Weight = 100
-const val Max_Volume = 100
+const val MIN_WEIGHT = 35
+const val MAX_WEIGHT = 100
+const val MAX_VOLUME = 100
 
 fun main() {
     var weight = 20
     var volume = 80
     println("The cargo with a weight of $weight kg and a volume of $volume liters corresponds to the category 'Average': " +
-            "${(weight > Min_Weight) and (weight <= Max_Weight) and  (volume < Max_Volume)}"
+            "${(weight > MIN_WEIGHT) and (weight <= MAX_WEIGHT) and  (volume < MAX_VOLUME)}"
     )
     weight = 50
     volume = 100
     println("The cargo with a weight of $weight kg and a volume of $volume liters corresponds to the category 'Average': " +
-            "${(weight > Min_Weight) and (weight <= Max_Weight) and  (volume < Max_Volume)}"
+            "${(weight > MIN_WEIGHT) and (weight <= MAX_WEIGHT) and  (volume < MAX_VOLUME)}"
     )
 }
