6794dc9f28cbaed1548e94b2b4f23a65d4118005
rina <iraledy@yandex.ru>
2026-01-17 22:23:36 +0300

    KS-4-2 Исправление задачи 2 к Уроку 4

--- src/main/kotlin/lesson_4/lesson4_task2.kt
+++ src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -7,12 +7,14 @@ const val MAX_VOLUME = 100
 fun main() {
     var weight = 20
     var volume = 80
-    println("The cargo with a weight of $weight kg and a volume of $volume liters corresponds to the category 'Average': " +
-            "${(weight > MIN_WEIGHT) and (weight <= MAX_WEIGHT) and  (volume < MAX_VOLUME)}"
+    println(
+        "The cargo with a weight of $weight kg and a volume of $volume liters corresponds to the category 'Average': " +
+                "${(weight > MIN_WEIGHT) && (weight <= MAX_WEIGHT) && (volume < MAX_VOLUME)}"
     )
     weight = 50
     volume = 100
-    println("The cargo with a weight of $weight kg and a volume of $volume liters corresponds to the category 'Average': " +
-            "${(weight > MIN_WEIGHT) and (weight <= MAX_WEIGHT) and  (volume < MAX_VOLUME)}"
+    println(
+        "The cargo with a weight of $weight kg and a volume of $volume liters corresponds to the category 'Average': " +
+                "${(weight > MIN_WEIGHT) && (weight <= MAX_WEIGHT) && (volume < MAX_VOLUME)}"
     )
 }
