0217dda7f5f780be0114d45ee435b5e8544d90fc
rina <iraledy@yandex.ru>
2026-01-19 20:57:40 +0300

    KS-4-3 Исправление задачи 3 к Уроку 4

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,20 +1,15 @@
 package org.example.lesson_4
 
 const val FAVORABLE_AIR_HUMIDITY = 20
-const val NOT_FAVORABLE_TIME_YEAR = "winter"
-
+const val WINTER_SEASON = "winter"
 
 fun main() {
     val isWeatherSunny: Boolean = true
     val isAwningOpen: Boolean = true
     val airHumidity: Int = 20
     val timeYear: String = "winter"
-    println(
-        "Are the conditions favorable for legume growth now? " +
-                "${
-                    isWeatherSunny && isAwningOpen &&
-                            FAVORABLE_AIR_HUMIDITY == airHumidity &&
-                            NOT_FAVORABLE_TIME_YEAR != timeYear
-                }"
-    )
+    val areConditionsFavorable = isWeatherSunny && isAwningOpen &&
+            FAVORABLE_AIR_HUMIDITY == airHumidity &&
+            WINTER_SEASON != timeYear
+    println("Are the conditions favorable for legume growth now? $areConditionsFavorable")
 }
