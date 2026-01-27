a9a2c82dabcb178b42c804144faadf5565be83fc
rina <iraledy@yandex.ru>
2026-01-27 19:04:38 +0300

    KS-3-1 Исправление задачи 1 к Уроку 3

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_3
 
 fun main() {
-    val dayGreeting = "Good afternoon,"
-    val eveningGreeting = "Good evening,"
+    val dayGreeting = "Good afternoon"
+    val eveningGreeting = "Good evening"
     val name = "Irina"
 
-    var fullGreeting = "$dayGreeting $name!"
+    var fullGreeting = "$dayGreeting, $name!"
     println(fullGreeting)
-    fullGreeting = "$eveningGreeting $name!"
+    fullGreeting = "$eveningGreeting, $name!"
     println(fullGreeting)
 }
