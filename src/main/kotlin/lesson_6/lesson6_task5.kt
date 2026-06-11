83481e03cab3a78c8f9f9946ab50abbfd5944a36
Rinacoder <iraledy@yandex.ru>
2026-06-12 00:30:43 +0300

    KS-6-5 Решение задачи 5 к уроку 6

---
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_6
+
+fun main() {
+    var attempts = 3
+    while (attempts > 0){
+        val a = (1..9).random()
+        val b = (1..9).random()
+        println("Сколько будет $a + $b?")
+        if (readln().toInt() == a + b){
+            println("Добро пожаловать!")
+            break
+        }
+        attempts--
+        if (attempts > 0) {
+            println("Неверно! Осталось попыток: $attempts")
+        } else {
+            println("Доступ запрещен")
+        }
+    }
+}
