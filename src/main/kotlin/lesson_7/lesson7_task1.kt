06a86cbc8fc2f0024d46d3c770d404309fd4772d
rina <iraledy@yandex.ru>
2026-06-14 01:12:34 +0300

    KS-7-1  Решение задачи 1 к уроку 7

---
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -0,0 +1,15 @@
+﻿package org.example.lesson_7
+
+fun main() {
+    val password = StringBuilder()
+
+    for (i in 1..6) {
+        if (i % 2 != 0) {
+            password.append(('a'..'z').random())
+        } else {
+            password.append(('0'..'9').random())
+        }
+    }
+
+    println(password)
+}
