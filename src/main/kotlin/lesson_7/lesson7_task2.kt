7deda6e9d85796872ff1e656ef0a5da68e1e2348
rina <iraledy@yandex.ru>
2026-06-16 02:17:43 +0300

    KS-7-2  Решение задачи 2 к уроку 7

---
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -0,0 +1,16 @@
+﻿package org.example.lesson_7
+
+fun main() {
+
+    while (true) {
+        val code = (1000..9999).random()
+        println("Ваш код авторизации: $code")
+        println("Введите код авторизации:")
+
+        if (readln().toInt() == code) {
+            println("Здравствуйте!")
+            break
+        }
+
+    }
+}
