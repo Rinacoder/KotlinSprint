3b4a0c822d0910b865db2d4fff23cb4a3285cc01
rina <iraledy@yandex.ru>
2026-06-16 02:50:11 +0300

    KS-7-4 Решение задачи 4 к уроку 7

---
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -0,0 +1,13 @@
+﻿    package org.example.lesson_7
+
+    fun main() {
+        println("Введите количество секунд:")
+        val allSeconds = readln().toInt()
+
+        for (second in allSeconds downTo 0){
+            println("Осталось $second секунд")
+            Thread.sleep(1000)
+        }
+
+        println("Время вышло")
+    }
