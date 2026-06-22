47c349e1dd05e060a1954da4afb79ed649401259
rina <iraledy@yandex.ru>
2026-06-23 02:43:43 +0300

    KS-7-5 Решение задачи 5 к уроку 7

---
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -0,0 +1,25 @@
+﻿package org.example.lesson_7
+
+fun main() {
+    println("Введите длину пароля не меньше 6")
+    val length = readln().toInt()
+    val password = StringBuilder()
+    val lowercase = 'a'..'z'
+    val uppercase = 'A'..'Z'
+    val digits = '0'..'9'
+    var range: IntRange
+
+    if (length >= 6) {
+        range = 1..length
+    } else {
+        range = 1..6
+    }
+    for (i in range) {
+        when ((0..2).random()) {
+            0 -> password.append(lowercase.random())
+            1 -> password.append(uppercase.random())
+            2 -> password.append(digits.random())
+        }
+    }
+    println(password)
+}
