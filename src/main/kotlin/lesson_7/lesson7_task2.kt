40b7e62daec0dea5180324b5950f470105d4535e
rina <iraledy@yandex.ru>
2026-06-23 02:13:59 +0300

    KS-7-2 Исправление задачи 2 к уроку 7

--- src/main/kotlin/lesson_7/lesson7_task2.kt
+++ src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,16 +1,13 @@
 ﻿package org.example.lesson_7
 
 fun main() {
+    val range = 1000..9999
 
-    while (true) {
-        val code = (1000..9999).random()
+    do {
+        val code = range.random()
         println("Ваш код авторизации: $code")
         println("Введите код авторизации:")
 
-        if (readln().toInt() == code) {
-            println("Здравствуйте!")
-            break
-        }
-
-    }
+    } while (readln().toInt() != code)
+    println("Здравствуйте!")
 }
