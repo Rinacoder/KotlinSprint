cfce157332a5a65c022a6a6f717ba9a315dbc921
rina <iraledy@yandex.ru>
2026-07-01 02:51:22 +0300

    KS-9-4 Решение задачи 4 к уроку 9

---
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -0,0 +1,13 @@
+﻿package org.example.lesson_9
+
+fun main() {
+    println("Введите список из 5 ингредиентов, перечисленных через запятую с пробелом")
+    val ingredients = readLine()?.split(", ")
+    if (ingredients?.size == 5) {
+        val sortedIngredients = ingredients.sorted()
+        println(sortedIngredients.joinToString())
+    } else {
+        println("Введено неверное количество ингредиентов")
+    }
+}
+
