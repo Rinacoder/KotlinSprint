aa28b9a5d4c0f9100d13536e1c4c81285a6e29e1
rina <iraledy@yandex.ru>
2026-07-01 03:59:01 +0300

    KS-9-5 Решение задачи 5 к уроку 9

---
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -0,0 +1,22 @@
+﻿package org.example.lesson_9
+
+fun main() {
+    val ingredients = mutableListOf<String>()
+
+    for (i in 1..5) {
+        println("Введите название ингредиента")
+        val ingredient = readln().lowercase()
+        if (ingredient !in ingredients && ingredient.isNotBlank()) {
+            ingredients.add(ingredient)
+        }
+    }
+
+    if (ingredients.isNotEmpty()) {
+        val sortedIngredients = ingredients.sorted().mapIndexed { index, item ->
+            if (index == 0) item.replaceFirstChar { it.uppercase() } else item
+        }
+        println(sortedIngredients.joinToString(", "))
+    } else {
+        println("Нет введенных ингредиентов")
+    }
+}
