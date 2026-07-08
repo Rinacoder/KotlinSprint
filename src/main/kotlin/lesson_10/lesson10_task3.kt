43d342d701661b4eebb35a6dfa6fbaaee4a0620b
rina <iraledy@yandex.ru>
2026-07-08 18:35:53 +0300

    KS-10-3 Решение задачи 3 к уроку 10

---
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -0,0 +1,19 @@
+﻿package org.example.lesson_10
+
+fun main() {
+    println("Введите длину пароля")
+    val password = passwordGenerator(readln().toInt())
+    println("Пароль: $password")
+}
+
+fun passwordGenerator(length: Int): String {
+    val password = mutableListOf<String>()
+    val elements = mutableListOf("!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " ")
+    for (i in 1..length) {
+        when {
+            i % 2 != 0 -> password.add((0..9).random().toString())
+            else -> password.add((elements).random())
+        }
+    }
+    return password.joinToString("")
+}
