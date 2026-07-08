23f7bb7671825345f1a2c32c41397a68a0c05987
rina <iraledy@yandex.ru>
2026-07-08 18:36:30 +0300

    KS-10-4 Решение задачи 4 к уроку 10

---
+++ src/main/kotlin/lesson_10/lesson10_task4.kt
@@ -0,0 +1,28 @@
+﻿package org.example.lesson_10
+
+fun main() {
+    var keepPlaying  = true
+    var playerWins = 0
+    while (keepPlaying ) {
+        val rollUser = rollDice()
+        println("Ход Игрока: $rollUser")
+        val rollComp = rollDice()
+        println("Ход Компьютера: $rollComp")
+        when {
+            rollUser > rollComp -> {
+                println("Выиграл игрок")
+                playerWins++
+            }
+
+            rollUser < rollComp -> println("Выиграл компьютер")
+            else -> println("Ничья")
+        }
+        println("Хотите бросить кости еще раз? Введите Да или Нет")
+        keepPlaying  = chekPlay(readln().lowercase())
+    }
+    println("Выиграно $playerWins партий")
+}
+
+fun rollDice(): Int = (1..6).random()
+
+fun chekPlay(answer: String = "да"): Boolean = answer == "да"
